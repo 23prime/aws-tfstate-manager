4ce6235c3699acc33415b143c813a5373f2b3370
23prime <23.prime.37@gmail.com>
2025-10-05 23:36:35 +0900

    Add project: tfstate-23prime-xyz

--- terraform/projects.tf
+++ terraform/projects.tf
@@ -7,5 +7,13 @@ locals {
         Project     = "terraform-bootstrap-aws"
       }
     }
+
+    "23prime-xyz" = {
+      bucket_name = "tfstate-23prime-xyz-${var.aws_account_id}"
+      tags = {
+        Environment = "production"
+        Project     = "23prime-xyz"
+      }
+    }
   }
 }
